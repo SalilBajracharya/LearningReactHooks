c5176e975ebee42c8a1326aea157f1305bdeadd0
SalilBajracharya <salil.bajracharya911@gmail.com>
2023-05-26 00:02:12 +0545

    In case of objects as State. - in this case, updating count replaces the whole object - watch result while incrementing and decrement. The theme gets removed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, {useState} from 'react';
 
-function InitialCount(){
-  console.log("This runs everytime !");
-  return 0;
-}
+// function InitialCount(){
+//   console.log("This runs everytime !");
+//   return 0;
+// }
 
 function App() {
-  const [count, setCount] = useState(() => InitialCount());
+  // const [count, setCount] = useState(() => InitialCount());
+  const [state, setState] = useState({count: 0, theme: 'red'});
+  const count = state.count;
+  const theme = state.theme;
 
   function decrementCount(){
-    setCount((prevCount) => prevCount-1);
+    setState((prevCount) => {
+       return { count: prevCount.count - 1};
+    });
   }
 
   function incrementCount(){
-    setCount(prevCount => prevCount+1);
+    setState(prevCount => {
+      return { count: prevCount.count + 1};
+    });
   }
 
   return (
     <>
       <button onClick={decrementCount}>-</button>
       <span>{count}</span>
+      <span>{theme}</span>
       <button onClick={incrementCount}>+</button>
     </>
   );
