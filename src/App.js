de28ae4f840afbc9b96e6f98ec6ff2dc7e25499f
SalilBajracharya <salil.bajracharya911@gmail.com>
2023-05-25 23:51:00 +0545

    * Counter created * Added useState to Get Count * Added decrementCount and incrementCount functions.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function decrementCount(){
+    setCount((prevCount) => prevCount-1);
+  }
+
+  function incrementCount(){
+    setCount(prevCount => prevCount+1);
+  }
+
   return (
     <>
-    <button>-</button>
-    <span>0</span>
-    <button>+</button>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
     </>
   );
 }
